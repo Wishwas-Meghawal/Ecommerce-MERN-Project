1599298daa7efd1222e9a997558c4e585bc22178
Wishwas <wishwasmeghawal1441@gmail.com>
2026-03-20 10:53:04 +0530

    feat(product): add routes for size and weight management

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import auth from '../middlewares/auth.js';
 import upload from '../middlewares/multer.js';
-import { createProduct, createProductRAMS, deleteMultipleProduct, deleteMultipleProductRAMS, deleteProduct, deleteProductRAMS, getAllFeaturedProducts, getAllProducts, getAllProductsByCatId, getAllProductsByCatName, getAllProductsByPrice, getAllProductsByRating, getAllProductsBySubCatId, getAllProductsBySubCatName, getAllProductsByThirdLavelCatId, getAllProductsByThirdLavelCatName, getProduct, getProductRAMS, getProductRAMSById, getProductsCount, removeImageFromCloudinary, updateProduct, updateProductRAMS, uploadImages } from '../controllers/product.controller.js';
+import { createProduct, createProductRAMS, createProductSIZE, createProductWEIGHT, deleteMultipleProduct, deleteMultipleProductRAMS, deleteMultipleProductSIZE, deleteMultipleProductWEIGHT, deleteProduct, deleteProductRAMS, deleteProductSIZE, deleteProductWEIGHT, getAllFeaturedProducts, getAllProducts, getAllProductsByCatId, getAllProductsByCatName, getAllProductsByPrice, getAllProductsByRating, getAllProductsBySubCatId, getAllProductsBySubCatName, getAllProductsByThirdLavelCatId, getAllProductsByThirdLavelCatName, getProduct, getProductRAMS, getProductRAMSById, getProductsCount, getProductSIZE, getProductSIZEById, getProductWEIGHT, getProductWEIGHTById, removeImageFromCloudinary, updateProduct, updateProductRAMS, updateProductSIZE, updateProductWEIGHT, uploadImages } from '../controllers/product.controller.js';
 
 
 const productRouter = Router();
@@ -38,6 +38,22 @@ productRouter.delete('/productRAMS/:id',deleteProductRAMS);
 productRouter.put("/productRAMS/:id", auth, updateProductRAMS);
 productRouter.get("/productRAMS/get" ,getProductRAMS);
 productRouter.get("/productRAMS/:id" ,getProductRAMSById);
+
+//Product Size Route
+productRouter.post("/productSIZE/create", auth, createProductSIZE);
+productRouter.delete('/productSIZE/deleteMultipleSize',deleteMultipleProductSIZE);
+productRouter.delete('/productSIZE/:id',deleteProductSIZE);
+productRouter.put("/productSIZE/:id", auth, updateProductSIZE);
+productRouter.get("/productSIZE/get" ,getProductSIZE);
+productRouter.get("/productSIZE/:id" ,getProductSIZEById);
+
+//Product Weigth Route
+productRouter.post("/productWEIGHT/create", auth, createProductWEIGHT);
+productRouter.delete('/productWEIGHT/deleteMultipleWeight',deleteMultipleProductWEIGHT);
+productRouter.delete('/productWEIGHT/:id',deleteProductWEIGHT);
+productRouter.put("/productWEIGHT/:id", auth, updateProductWEIGHT);
+productRouter.get("/productWEIGHT/get" ,getProductWEIGHT);
+productRouter.get("/productWEIGHT/:id" ,getProductWEIGHTById);
 
 
 
